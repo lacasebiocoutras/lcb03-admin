0d1131a8300b3e1a4fdbdf819333df2f840caee4
lacasebiocoutras <lacasebiocoutras.adm@gmail.com>
2016-12-28 21:46:16 +0000

    build

--- deploy_init.sh
+++ deploy_init.sh
@@ -2,7 +2,7 @@
 set -e
 echo "$TRAVIS_BRANCH"
 echo "$TRAVIS_PULL_REQUEST"
-if [ "$TRAVIS_PULL_REQUEST" == "false" -o "$TRAVIS_BRANCH" != "master" ]
+if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "master" ]
 then 
   echo "This pullrequest was not made on master! No deploy!" 
   exit 1
